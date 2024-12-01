f7e394f27d9e4a58507d360019782dc984e2ecc9
Roman Steiner <roman@toastlab.ch>
2024-12-01 12:12:36 +0100

    measure time

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+use std::time::Instant;
+
 mod d01;
 
 fn main() {
     let input = include_str!("../inputs/d01.txt");
-    println!("Part 1: {}", d01::part1(input));
-    println!("Part 2: {}", d01::part2(input));
+
+    let start_part1 = Instant::now();
+    let result_part1 = d01::part1(input);
+    let duration_part1 = start_part1.elapsed();
+    println!("Part 1: {} (took {:?})", result_part1, duration_part1);
+
+    let start_part2 = Instant::now();
+    let result_part2 = d01::part2(input);
+    let duration_part2 = start_part2.elapsed();
+    println!("Part 2: {} (took {:?})", result_part2, duration_part2);
 }
