278a66718a2e5d076bed4fb44dd6bb3be297328d
Roman Steiner <roman@toastlab.ch>
2024-12-03 11:46:10 +0100

    refactor regex usage to utilize LazyLock for improved performance

--- src/d03.rs
+++ src/d03.rs
@@ -1,5 +1,9 @@
+use std::sync::LazyLock;
+
+static MULREG: LazyLock<regex::Regex> = LazyLock::new(|| regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap());
+
 pub fn part1(input: &String) -> i32 {
-    let r = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let r = &*MULREG;
     let mut sum = 0;
 
     for (_, [a, b]) in r.captures_iter(&input).map(|c| c.extract()) {
@@ -12,12 +16,10 @@ pub fn part1(input: &String) -> i32 {
 }
 
 pub fn part2(input: &String) -> i32 {
-    let split_by_do = regex::Regex::new(r"do\(\)").unwrap();
-    let split_by_dont = regex::Regex::new(r"don't\(\)").unwrap();
     let mut sum = 0;
     
-    for chunk in split_by_do.split(&input) {
-        let before_dont = split_by_dont.split(&chunk).next().unwrap();
+    for chunk in input.split("do()") {
+        let before_dont = chunk.split("don't()").next().unwrap();
         sum += part1(&String::from(before_dont));
     }
 
